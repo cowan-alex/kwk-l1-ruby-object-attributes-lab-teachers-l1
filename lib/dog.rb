df10c11eae42340a2e8071bd026539a803ebff10
Alex Cowan <alexandria.cowan.17@tfacorps.org>
2018-06-22 16:59:26 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # dog.rb
+class Dog 
+  def name 
+    return @name
+  end
+  
+  def name=(dogs_name)
+    @name= dogs_name
+  end
+  
+  def breed 
+    return @breed
+  end
+  
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+end
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,18 @@
 # person.rb
+class Person 
+  def name 
+    return @name
+  end
+  
+  def name=(persons_name)
+    @name = persons_name
+  end
+  
+  def job
+      return @job
+    end
+    
+  def job=(persons_job)
+    @job = persons_job
+  end
+end
